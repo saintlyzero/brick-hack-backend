791ae52cbc15296e03fa1bfac9f5e07de3a6217d
Navneet Desai <navneetdesai44@gmail.com>
2023-02-25 21:30:50 -0500

    Fix issue: SummaryConfig app

--- lastminute/lastminute/settings.py
+++ lastminute/lastminute/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'summary.apps.SummaryConfig'
+    'summary.apps.SummaryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
