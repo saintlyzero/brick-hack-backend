4d8f3b3b92e49e84981ffc113e670121d981f1c9
Navneet Desai <navneetdesai44@gmail.com>
2023-02-25 22:31:37 -0500

    Remove django key add more files to .gitignore

--- lastminute/lastminute/settings.py
+++ lastminute/lastminute/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from . import django_auth
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+xt_%zqw1%@e4ufl_u^8r8p^rxsbcny_nkwxl-d3dl3u@81mwt'
+SECRET_KEY = django_auth.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
